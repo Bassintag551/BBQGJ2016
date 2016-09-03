848b8e898850df1c9e3b59ed97ed2a931221a4af
Ben <philippine@bonbonpiment.com>
2016-09-04 00:26:10 +0200

    Character selection implementation

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour {
-
-    public PlayerSelector[] characterAvailable;
-    public int[] characterIndex;
-    public GameObject characterSelected;
+    
+    public Texture2D[] charactersIcons;
+    public Transform[] characters;
+    private bool[] selected;
+    public RawImage[] selectors;
 
 	// Use this for initialization
 	void Start () {
-       characterIndex = new int[4];
+        selected = new bool[4];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        foreach(bool b in ControllerManager.Instance.Active)
+        for (int i = 0; i < 4; i++)
         {
+            bool b = ControllerManager.Instance.Active[i];
             if (!b)
                 continue;
-
+            else if (ControllerManager.Instance.A[i])
+            {
+                selected[i] = true;
+            }
         }
 	}
 }
