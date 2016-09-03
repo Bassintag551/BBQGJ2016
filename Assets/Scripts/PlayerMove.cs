65749720a691d3ed00d8231d25d86fe7a22334c9
Loïc M <muraloic0@gmail.com>
2016-09-03 17:36:28 +0200

    Add script to move player

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	public uint joystickId = 1;
+	public int speed = 1;
+
+	private Vector3 movement;
+
+	void FixedUpdate() {
+		float h = Input.GetAxis("H" + joystickId.ToString());
+		float v = Input.GetAxis("V" + joystickId.ToString());
+
+		movement = new Vector3(h, v, 0);
+		movement.Normalize();
+
+		transform.Translate(movement * speed * Time.deltaTime);
+	}
+}
