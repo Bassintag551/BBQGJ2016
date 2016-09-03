9bc9079fedcaae34590ee3d614a5c70fcc3d2c63
Antoine <antoine.stempfer@gmail.com>
2016-09-03 19:24:53 +0200

    Added Trail and collision detection

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,8 @@ public class PlayerMove : MonoBehaviour {
 	public uint joystickId = 1;
 	public int speed = 1;
 
+    public float angle { private set; get; }
+
 	private Vector3 movement;
 
 	void FixedUpdate() {
@@ -13,6 +15,8 @@ public class PlayerMove : MonoBehaviour {
 
 		movement = new Vector3(h, v, 0);
 		movement.Normalize();
+
+        angle = Vector3.Angle(Vector3.up, movement);
 
 		transform.Translate(movement * speed * Time.deltaTime);
 	}
