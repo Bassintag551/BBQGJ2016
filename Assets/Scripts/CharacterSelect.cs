fa96784bba8900f23ea51907bd403a06b5a01d08
Ben <philippine@bonbonpiment.com>
2016-09-03 22:24:57 +0200

    Begin of character selector implementation

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour {
+
+    public PlayerSelector[] characterAvailable;
+    public int[] characterIndex;
+    public GameObject characterSelected;
+
+	// Use this for initialization
+	void Start () {
+       characterIndex = new int[4];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
