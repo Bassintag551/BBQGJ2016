3a89be88589c5bfcd69641dd98dd25dbce58f971
Loïc M <muraloic0@gmail.com>
2016-09-04 12:17:54 +0200

    Avoid explosive cheese on nothing

---
+++ Assets/Scripts/ExplosiveCheese.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosiveCheese : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D collider) {
+		if(collider.GetComponent<Cutout>()) {
+			Destroy(gameObject);
+		}
+	}
+}
